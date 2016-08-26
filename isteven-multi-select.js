104cb443c2372c311a89fddad1c6c5718cb92de2
vishnurs <rsvishnuu@gmail.com>
2016-08-26 18:11:26 +0530

    typo

--- isteven-multi-select.js
+++ isteven-multi-select.js
@@ -9,7 +9,7 @@
  * --------------------------------------------------------------------------------
  * The MIT License (MIT)
  *
- * Copyright (c) 2014 Ignatius Steven (https://github.c7om/isteven)
+ * Copyright (c) 2014 Ignatius Steven (https://github.com/isteven)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
